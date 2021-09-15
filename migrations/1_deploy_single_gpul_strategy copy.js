705f3de237c52f5a81a2a43a423281cc1d7c9610
Richard Jamieson <jamieson.rich@gmail.com>
2021-09-15 13:08:34 +0200

    add GPUL strategies

--- migrations/1_deploy_single_gpul_strategy copy.js
+++ migrations/1_deploy_single_gpul_strategy copy.js
@@ -4,8 +4,8 @@ const { gpulVaults } = require('../configs/gpulVaults');
 module.exports = async function (deployer, network) {
     await deployer.deploy(
         StrategyMasterHealer,
-        gpulVaults[1].addresses, // configuration addresses: vaulthealer, masterchef, unirouter, want, earned
-        ...gpulVaults[1].strategyConfig // all other contract configuration variables
+        gpulVaults[0].addresses, // configuration addresses: vaulthealer, masterchef, unirouter, want, earned
+        ...gpulVaults[0].strategyConfig // all other contract configuration variables
     )
     const StrategyMasterHealerInstance = await StrategyMasterHealer.deployed();
 
