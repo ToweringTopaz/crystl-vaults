17ebaeba5021fdef6605b11d27c867175836a9fd
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2021-10-01 16:56:10 -0500

    improve PathStorage

--- contracts/PathStorage.sol
+++ contracts/PathStorage.sol
@@ -7,29 +7,86 @@ abstract contract PathStorage {
     uint constant private MAX_PATH = 5;
     mapping(bytes32 => address[MAX_PATH]) private _paths;
     
+    event SetPath(address[MAX_PATH] path);
+    
     function _setPath(address[] memory _path) internal {
-        assert(_path.length > 0 && _path.length <= MAX_PATH);
-        
-        bytes32 hashAB = keccak256(abi.encodePacked(_path[0],_path[_path.length - 1]));
-        bytes32 hashBA = keccak256(abi.encodePacked(_path[_path.length - 1],_path[0]));
-        for(uint i; i < _path.length; i++) {
-            _paths[hashAB][i] = _path[i];
-            _paths[hashBA][_path.length - 1 - i] = _path[i];
-        }
-    }
-    function getPath(address a, address b) public view returns (address[] memory path) {
-        bytes32 hashAB = keccak256(abi.encodePacked(a, b));
-        address[MAX_PATH] storage _path = _paths[hashAB];
-        path = new address[](MAX_PATH);
-        
+        require(_path.length <= MAX_PATH, "invalid _path.length");
+        uint len = MAX_PATH;
         for (uint i; i < MAX_PATH; i++) {
-            path[i] = _path[i];
-            if (path[i] == address(0)) {
-                assembly { path := i } //reduce length
+            if (_path[i] == address(0)) {
+                len = i;
+                for (uint j = i+1; j < MAX_PATH; j++) {
+                    require(_path[j] == address(0), "broken path");
+                }
                 break;
             }
         }
+        
+        bytes32 hashAB = keccak256(abi.encodePacked(_path[0],_path[len - 1]));
+        bytes32 hashBA = keccak256(abi.encodePacked(_path[len - 1],_path[0]));
+        address[MAX_PATH] storage pathAB = _paths[hashAB];
+        address[MAX_PATH] storage pathBA = _paths[hashBA];
+        for(uint i; i < len; i++) {
+            pathAB[i] = _path[i];
+            pathBA[len - 1 - i] = _path[i];
+        }
+        emit SetPath(pathAB);
+        emit SetPath(pathBA);
+        
+        //recursively fill sub-paths
+        if (len > 2) {
+            _autoSubPath(_shl(pathAB, len), len - 1);
+            _autoSubPath(_shl(pathBA, len), len - 1);
+        }
+    }
+    function _autoSubPath(address[MAX_PATH] memory _path, uint len) private {
+        
+        bytes32 hashAB = keccak256(abi.encodePacked(_path[0],_path[len - 1]));
+        address[MAX_PATH] storage pathAB = _paths[hashAB];
+        
+        if (pathAB[0] == address(0)) { //don't replace paths already defined
+            bytes32 hashBA = keccak256(abi.encodePacked(_path[len - 1],_path[0]));
+            address[MAX_PATH] storage pathBA = _paths[hashBA];
+            for(uint i; i < _path.length; i++) {
+                pathAB[i] = _path[i];
+                pathBA[len - 1 - i] = _path[i];
+            }
+            emit SetPath(pathAB);
+            emit SetPath(pathBA);
+            
+            //recursively fill sub-paths
+            if (len > 2) {
+                _autoSubPath(_shl(pathAB, len), len - 1);
+                _autoSubPath(_shl(pathBA, len), len - 1);
+            }
+        }
+    }
+    function _shl(address[MAX_PATH] memory _path, uint len) private pure returns (address[MAX_PATH] memory path) {
+        for (uint i; i < len - 1; i++) {
+            path[i] = _path[i+1];
+        }
+    }
+    function _len(address[MAX_PATH] memory _path) private pure returns (uint len) {
+        for (uint i; i < MAX_PATH; i++) {
+            if (_path[i] == address(0)) return i;
+        }
+        return MAX_PATH;
+    }
+    function getPath(address a, address b) public view returns (address[] memory path) {
+        if (a == b) {
+            path = new address[](1);
+            path[0] = a;
+            return path;
+        }
+        bytes32 hashAB = keccak256(abi.encodePacked(a, b));
+        address[MAX_PATH] storage _path = _paths[hashAB];
+        path = new address[](_len(_path));
         require(path.length > 0, "path not found");
+        
+        for (uint i; i < path.length; i++) {
+            path[i] = _path[i];
+        }
+        
     }
     
 }
