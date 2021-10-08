e02d502559c87b4ff3b1edc2a578695000b2895c
Richard Jamieson <jamieson.rich@gmail.com>
2021-10-08 12:06:11 +0200

    fixed typo, added console.logs

--- contracts/BaseStrategySwapLogic.sol
+++ contracts/BaseStrategySwapLogic.sol
@@ -9,6 +9,8 @@ import "./libs/PrismLibrary2.sol";
 import "./BaseStrategy.sol";
 import "./Magnetite.sol";
 
+import "hardhat/console.sol";
+
 //Contains the strategy's functions related to swapping, earning, etc.
 abstract contract BaseStrategySwapLogic is BaseStrategy {
     using SafeERC20 for IERC20;
@@ -82,13 +84,16 @@ abstract contract BaseStrategySwapLogic is BaseStrategy {
     function _earn(address _to) internal whenEarnIsReady {
         
         uint wantBalanceBefore = _wantBalance(); //Don't touch starting want balance (anti-rug)
-        
+        console.log("Harvesting now");
         _vaultHarvest(); // Harvest farm tokens
-    
+        console.log("Harvesting done");
+
         // Converts farm tokens into want tokens
         //Try/catch means we carry on even if compounding fails for some reason
         try this._swapEarnedToWant(_to, wantBalanceBefore) returns (bool) {
+                console.log("Farming now");
                 _farm(); //deposit the want tokens so they can begin earning
+                console.log("Farming done");
         } catch (bytes memory e) {//Generate a log on failure
             uint[] memory earnBals = new uint[](lpTokenLength);
             for (uint i; i < lpTokenLength; i++) {
@@ -105,29 +110,37 @@ abstract contract BaseStrategySwapLogic is BaseStrategy {
 
         for (uint i; i < earnedLength; i++) { //Process each earned token, whether it's 1, 2, or 8.
             address earnedAddress = earned[i];
-            
+            console.log(earnedAddress);
             uint256 earnedAmt = IERC20(earnedAddress).balanceOf(address(this));
+            console.log(earnedAmt);
+
             if (earnedAddress == wantAddress)
                 earnedAmt -= _wantBal; //ignore pre-existing want tokens
-            
+
             uint dust = settings.dust; //minimum number of tokens to bother trying to compound
     
             if (earnedAmt > dust) {
+                console.log("earnedAmt>dust");
                 success = true; //We have something worth compounding
                 earnedAmt = distributeFees(earnedAddress, earnedAmt, _to); // handles all fees for this earned token
-                
+                console.log(earnedAmt);
                 // Swap half earned to token0, half to token1 (or split evenly however we must, for balancer etc)
                 // Same logic works if lpTokenLength == 1 ie single-staking pools
-                for (uint j; j < lpTokenLength; i++) {
+                for (uint j; j < lpTokenLength; j++) {
+                    console.log("made it into the for loop");
                     _safeSwap(earnedAmt / lpTokenLength, earnedAddress, lpToken[j], address(this));
+                    console.log(j);
                 }
             }
         }
+        console.log(success);
         require(success, "dust-earnedToLP");
         //lpTokenLength == 1 means single-stake, not LP
         if (lpTokenLength > 1) {
             // Get want tokens, ie. add liquidity
+            console.log("About to mint");
             PrismLibrary2.optimalMint(wantAddress, lpToken[0], lpToken[1]);
+            console.log("Minted");
         }
     }
     
@@ -236,7 +249,7 @@ abstract contract BaseStrategySwapLogic is BaseStrategy {
         if (wantAmt == 0) return;
         
         uint256 sharesBefore = vaultSharesTotal();
-        
+        console.log("About to deposit want back into vault");
         _vaultDeposit(wantAmt); //approves the transfer then calls the pool contract to deposit
         uint256 sharesAfter = vaultSharesTotal();
         
