d2efe3e3555fbeb75e82593ad1f57fb37ca7cbc1
Richard Jamieson <jamieson.rich@gmail.com>
2021-11-05 06:56:31 +0200

    config updates

--- migrations/1_deploy_single_strategy.js
+++ migrations/1_deploy_single_strategy.js
@@ -4,12 +4,12 @@ const { cronaswapVaults } = require('../configs/cronaswapVaults');
 module.exports = async function (deployer, network) {
     await deployer.deploy(
         StrategyMasterHealer,
-        cronaswapVaults[1].addresses, // configuration addresses: vaulthealer, masterchef, unirouter, want, earned
-        ...cronaswapVaults[1].strategyConfig // all other contract configuration variables
+        cronaswapVaults[0].addresses, // configuration addresses: vaulthealer, masterchef, unirouter, want, earned
+        ...cronaswapVaults[0].strategyConfig // all other contract configuration variables
     )
-    const WcroUsdcStrategyInstance = await StrategyMasterHealer.deployed();
+    const WcroCronaStrategyInstance = await StrategyMasterHealer.deployed();
 
     console.table({
-        WcroUsdcStrategy: WcroUsdcStrategyInstance.address,
+        WcroCronaStrategy: WcroCronaStrategyInstance.address,
     })
 };
--- truffle-config.js
+++ truffle-config.js
@@ -53,12 +53,12 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.8.4",    // Fetch exact version from solc-bin (default: truffle's version)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //   optimizer: {
-      //     enabled: true,
-      //     runs: 200
-      //   },
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+      }
     },
   },
   db: {
