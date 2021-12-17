e639de2c5f1b55677991f916caf4ca19c1e1bcda
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2021-12-17 03:40:30 -0600

    Update IUniswapV2Pair.sol

--- contracts/libs/IUniswapV2Pair.sol
+++ contracts/libs/IUniswapV2Pair.sol
@@ -1,20 +1,9 @@
 // SPDX-License-Identifier: GPLv2
 pragma solidity >=0.5.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-interface IUniswapV2Pair is IERC20 {
-
-  function name() external pure returns (string memory);
-  function symbol() external pure returns (string memory);
-  function decimals() external pure returns (uint8);
-  function totalSupply() external view returns (uint);
-  function balanceOf(address owner) external view returns (uint);
-  function allowance(address owner, address spender) external view returns (uint);
-
-  function approve(address spender, uint value) external returns (bool);
-  function transfer(address to, uint value) external returns (bool);
-  function transferFrom(address from, address to, uint value) external returns (bool);
+interface IUniswapV2Pair is IERC20Metadata {
 
   function DOMAIN_SEPARATOR() external view returns (bytes32);
   function PERMIT_TYPEHASH() external pure returns (bytes32);
