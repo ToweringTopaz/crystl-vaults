659cc2e272c266a19707a2dff1755bddb77947d4
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2021-12-23 04:13:27 -0600

    vaults are "vaults" represented by "vids"

--- contracts/VaultHealerBase.sol
+++ contracts/VaultHealerBase.sol
@@ -14,37 +14,37 @@ abstract contract VaultHealerBase is Ownable, ERC1155Supply, ReentrancyGuard {
     using LibVaultConfig for VaultFees;
     using BitMaps for BitMaps.BitMap;
 
-    struct PoolInfo {
+    struct VaultInfo {
         IERC20 want; //  want token.
         IStrategy strat; // Strategy contract that will auto compound want tokens
         VaultFee withdrawFee;
         uint256 accRewardTokensPerShare;
         uint256 balanceCrystlCompounderLastUpdate;
-        uint256 targetPid; //maximizer target, which accumulates tokens
+        uint256 targetVid; //maximizer target, which accumulates tokens
         mapping(address => uint256) rewardDebt;
         // bytes data;
     }
 
-    PoolInfo[] internal _poolInfo; // Info of each pool.
+    VaultInfo[] internal _vaultInfo; // Info of each vault.
     BitMaps.BitMap private pauseMap; //Boolean pause status for each vault; true == unpaused
     BitMaps.BitMap private _overrideDefaultEarnFees; // strategy's fee config doesn't change with the vaulthealer's default
     BitMaps.BitMap private _overrideDefaultWithdrawFee;
     VaultFees public defaultEarnFees; // Settings which are generally applied to all strategies
     VaultFee public defaultWithdrawFee; //withdrawal fee is set separately from earn fees
 
-    //pid for any of our strategies
+    //vid for any of our strategies
     mapping(address => uint) private _strats;
     
-    event AddPool(address indexed strat);
-    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
-    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event AddVault(address indexed strat);
+    event Deposit(address indexed user, uint256 indexed vid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed vid, uint256 amount);
     event SetDefaultEarnFees(VaultFees _earnFees);
-    event SetDefaultFail(uint pid);
-    event SetEarnFees(uint pid, VaultFees _earnFees);
-    event SetWithdrawFee(uint pid, VaultFee _withdrawFee);
-    event ResetEarnFees(uint pid);
-    event Paused(uint pid);
-    event Unpaused(uint pid);
+    event SetDefaultFail(uint vid);
+    event SetEarnFees(uint vid, VaultFees _earnFees);
+    event SetWithdrawFee(uint vid, VaultFee _withdrawFee);
+    event ResetEarnFees(uint vid);
+    event Paused(uint vid);
+    event Unpaused(uint vid);
     
     constructor(VaultFees memory _earnFees, VaultFee memory _withdrawFee) ERC1155("") {
         _earnFees.check();
@@ -52,40 +52,40 @@ abstract contract VaultHealerBase is Ownable, ERC1155Supply, ReentrancyGuard {
         defaultWithdrawFee = _withdrawFee;
         emit SetDefaultEarnFees(_earnFees);
 
-        _poolInfo.push(); //so uninitialized pid variables (pid 0) can be assumed as invalid
+        _vaultInfo.push(); //so uninitialized vid variables (vid 0) can be assumed as invalid
     }
     
-    function poolLength() external view returns (uint256) {
-        return _poolInfo.length;
+    function vaultLength() external view returns (uint256) {
+        return _vaultInfo.length;
     }
-    function poolInfo(uint pid) external view returns (IERC20 want, IStrategy strat) {
-        return (_poolInfo[pid].want, _poolInfo[pid].strat);
+    function vaultInfo(uint vid) external view returns (IERC20 want, IStrategy strat) {
+        return (_vaultInfo[vid].want, _vaultInfo[vid].strat);
     }
 
-    function rewardDebt(uint pid, address user) external view returns (uint) {
-        return _poolInfo[pid].rewardDebt[user];
+    function rewardDebt(uint vid, address user) external view returns (uint) {
+        return _vaultInfo[vid].rewardDebt[user];
     }
 
     // View function to see staked Want tokens on frontend.
-    function stakedWantTokens(uint256 _pid, address _user) external view returns (uint256) {
-        PoolInfo storage pool = _poolInfo[_pid];
+    function stakedWantTokens(uint256 _vid, address _user) external view returns (uint256) {
+        VaultInfo storage vault = _vaultInfo[_vid];
 
-        uint256 _sharesTotal = totalSupply(_pid); //balanceOf(_user, _pid);
-        uint256 wantLockedTotal = pool.strat.wantLockedTotal();
+        uint256 _sharesTotal = totalSupply(_vid); //balanceOf(_user, _vid);
+        uint256 wantLockedTotal = vault.strat.wantLockedTotal();
         if (_sharesTotal == 0) {
             return 0;
         }
-        return balanceOf(_user, _pid) * wantLockedTotal / _sharesTotal;
+        return balanceOf(_user, _vid) * wantLockedTotal / _sharesTotal;
     }
 
     // View function to see staked Want tokens on frontend.
-    function boostedWantTokens(uint256 _pid, address _user) external view returns (uint256) {
-        PoolInfo storage pool = _poolInfo[_pid];
-        if (pool.strat.boostPoolAddress() == address(0)) return 0;
+    function boostedWantTokens(uint256 _vid, address _user) external view returns (uint256) {
+        VaultInfo storage vault = _vaultInfo[_vid];
+        if (vault.strat.boostPoolAddress() == address(0)) return 0;
         
-        IBoostPool boostPool = IBoostPool(pool.strat.boostPoolAddress());
-        uint256 _sharesTotal = totalSupply(_pid);
-        uint256 wantLockedTotal = pool.strat.wantLockedTotal();
+        IBoostPool boostPool = IBoostPool(vault.strat.boostPoolAddress());
+        uint256 _sharesTotal = totalSupply(_vid);
+        uint256 wantLockedTotal = vault.strat.wantLockedTotal();
         if (_sharesTotal == 0) {
             return 0;
         }
@@ -93,98 +93,98 @@ abstract contract VaultHealerBase is Ownable, ERC1155Supply, ReentrancyGuard {
     }
 
     /**
-     * @dev Add a new want to the pool. Can only be called by the owner.
+     * @dev Add a new want to the vault. Can only be called by the owner.
      */
-    function addPool(address _strat) external onlyOwner nonReentrant {
+    function addVault(address _strat) external onlyOwner nonReentrant {
         require(!isStrat(_strat), "Existing strategy");
         IStrategy strat = IStrategy(_strat);
-        uint pid = _poolInfo.length;
-        _poolInfo.push();
-        PoolInfo storage pool = _poolInfo[pid];
-        pool.want = strat.wantToken();
-        pool.strat = strat;
-        pool.targetPid = _strats[address(strat.targetVault())];
+        uint vid = _vaultInfo.length;
+        _vaultInfo.push();
+        VaultInfo storage vault = _vaultInfo[vid];
+        vault.want = strat.wantToken();
+        vault.strat = strat;
+        vault.targetVid = _strats[address(strat.targetVault())];
         strat.setEarnFees(defaultEarnFees);
-        pool.withdrawFee = defaultWithdrawFee; //I've added this line in to set fees in the VH based pool as well as in the strat's vaultFees struct
-        // pool.boostPoolAddress = strat.boostPoolAddress();
+        vault.withdrawFee = defaultWithdrawFee; //I've added this line in to set fees in the VH based pool as well as in the strat's vaultFees struct
+        // vault.boostPoolAddress = strat.boostPoolAddress();
         
-        _strats[_strat] = pid;
-        _unpause(pid);
-        emit AddPool(_strat);
+        _strats[_strat] = vid;
+        _unpause(vid);
+        emit AddVault(_strat);
     }
     
     //enables sharesTotal function on strategy
     function sharesTotal(address _strat) external view returns (uint) {
-        uint pid = findPid(_strat);
-        return totalSupply(pid);
+        uint vid = findvid(_strat);
+        return totalSupply(vid);
     }
     function isStrat(address _strat) public view returns (bool) {
         return _strats[_strat] > 0;
     }
-    function findPid(address _strat) public view returns (uint) {
-        uint pid = _strats[_strat];
-        require(pid > 0, "address is not a strategy on this VaultHealer"); //must revert here for security
-        return pid;
+    function findvid(address _strat) public view returns (uint) {
+        uint vid = _strats[_strat];
+        require(vid > 0, "address is not a strategy on this VaultHealer"); //must revert here for security
+        return vid;
     }
     
-    function getEarnFees(uint _pid) public view returns (VaultFees memory) {
-        PoolInfo storage pool = _poolInfo[_pid];
-        if (overrideDefaultEarnFees(_pid)) 
-            return pool.strat.earnFees();
+    function getEarnFees(uint _vid) public view returns (VaultFees memory) {
+        VaultInfo storage vault = _vaultInfo[_vid];
+        if (overrideDefaultEarnFees(_vid)) 
+            return vault.strat.earnFees();
         else
             return defaultEarnFees;
     }
     
-    function overrideDefaultEarnFees(uint pid) public view returns (bool) { // strategy's fee config doesn't change with the vaulthealer's default
-        return _overrideDefaultEarnFees.get(pid);
+    function overrideDefaultEarnFees(uint vid) public view returns (bool) { // strategy's fee config doesn't change with the vaulthealer's default
+        return _overrideDefaultEarnFees.get(vid);
     }
-    function overrideDefaultWithdrawFee(uint pid) public view returns (bool) {
-        return _overrideDefaultWithdrawFee.get(pid);
+    function overrideDefaultWithdrawFee(uint vid) public view returns (bool) {
+        return _overrideDefaultWithdrawFee.get(vid);
     }
 
      function setDefaultEarnFees(VaultFees calldata _earnFees) external onlyOwner {
         defaultEarnFees = _earnFees;
         emit SetDefaultEarnFees(_earnFees);
         
-        for (uint i = 1; i < _poolInfo.length; i++) {
+        for (uint i = 1; i < _vaultInfo.length; i++) {
             if (overrideDefaultEarnFees(i)) continue; //todo: optimize use of bitmap, like earn
-            try _poolInfo[i].strat.setEarnFees(_earnFees) {}
+            try _vaultInfo[i].strat.setEarnFees(_earnFees) {}
             catch { emit SetDefaultFail(i); }
         }
     }   
-    function setEarnFees(uint _pid, VaultFees calldata _earnFees) external onlyOwner {
-        _overrideDefaultEarnFees.set(_pid);
-        _poolInfo[_pid].strat.setEarnFees(_earnFees);
-        emit SetEarnFees(_pid, _earnFees);
+    function setEarnFees(uint _vid, VaultFees calldata _earnFees) external onlyOwner {
+        _overrideDefaultEarnFees.set(_vid);
+        _vaultInfo[_vid].strat.setEarnFees(_earnFees);
+        emit SetEarnFees(_vid, _earnFees);
     }
-    function resetEarnFees(uint _pid) external onlyOwner {
-        _overrideDefaultEarnFees.unset(_pid);
-        _poolInfo[_pid].strat.setEarnFees(defaultEarnFees);
-        emit ResetEarnFees(_pid);
+    function resetEarnFees(uint _vid) external onlyOwner {
+        _overrideDefaultEarnFees.unset(_vid);
+        _vaultInfo[_vid].strat.setEarnFees(defaultEarnFees);
+        emit ResetEarnFees(_vid);
     }
     
-    function getWithdrawFee(uint _pid) public view returns (VaultFee memory) {
-        return _poolInfo[_pid].withdrawFee;
+    function getWithdrawFee(uint _vid) public view returns (VaultFee memory) {
+        return _vaultInfo[_vid].withdrawFee;
     }
 
-    function setWithdrawFee(uint _pid, VaultFee calldata _withdrawFee) external onlyOwner {
-        _overrideDefaultWithdrawFee.set(_pid);
-        _poolInfo[_pid].withdrawFee = _withdrawFee;
-        emit SetWithdrawFee(_pid, _withdrawFee);
+    function setWithdrawFee(uint _vid, VaultFee calldata _withdrawFee) external onlyOwner {
+        _overrideDefaultWithdrawFee.set(_vid);
+        _vaultInfo[_vid].withdrawFee = _withdrawFee;
+        emit SetWithdrawFee(_vid, _withdrawFee);
     }
     
     function earnAll() external nonReentrant {
 
-        uint bucketLength = (_poolInfo.length >> 8) + 1; // use one uint256 per 256 vaults
+        uint bucketLength = (_vaultInfo.length >> 8) + 1; // use one uint256 per 256 vaults
 
         for (uint i; i < bucketLength; i++) {
             uint earnMap = pauseMap._data[i]; //earn unpaused vaults
 
             uint end = (i+1) << 8; // buckets end at multiples of 256
-            if (_poolInfo.length < end) end = _poolInfo.length; //or if less, the final pool
+            if (_vaultInfo.length < end) end = _vaultInfo.length; //or if less, the final pool
             for (uint j = i << 8; j < end; j++) {
                 if (earnMap & 1 > 0) { //smallest bit is "true"
-                    try _poolInfo[j].strat.earn(_msgSender()) {}
+                    try _vaultInfo[j].strat.earn(_msgSender()) {}
                     catch {}
                 }
                 earnMap >>= 1; //shift away the used bit
@@ -193,15 +193,15 @@ abstract contract VaultHealerBase is Ownable, ERC1155Supply, ReentrancyGuard {
         }
     }
     
-    function earnSome(uint256[] memory pids) external nonReentrant {
+    function earnSome(uint256[] memory vids) external nonReentrant {
 
-        uint bucketLength = (_poolInfo.length >> 8) + 1; // use one uint256 per 256 vaults
-        uint256[] memory selBuckets = new uint256[](bucketLength); //BitMap of selected pids
+        uint bucketLength = (_vaultInfo.length >> 8) + 1; // use one uint256 per 256 vaults
+        uint256[] memory selBuckets = new uint256[](bucketLength); //BitMap of selected vids
 
-        for (uint i; i < pids.length; i++) { //memory bitmap of all selected pids
-            uint pid = pids[i];
-            if (pid <= _poolInfo.length)
-                selBuckets[pid >> 8] |= 1 << (pid & 0xff); //set bit for selected pid
+        for (uint i; i < vids.length; i++) { //memory bitmap of all selected vids
+            uint vid = vids[i];
+            if (vid <= _vaultInfo.length)
+                selBuckets[vid >> 8] |= 1 << (vid & 0xff); //set bit for selected vid
         }
 
         for (uint i; i < bucketLength; i++) {
@@ -210,7 +210,7 @@ abstract contract VaultHealerBase is Ownable, ERC1155Supply, ReentrancyGuard {
             uint end = (i+1) << 8; // buckets end at multiples of 256
             for (uint j = i << 8; j < end; j++) {//0-255, 256-511, ...
                 if (earnMap & 1 > 0) { //smallest bit is "true"
-                    try _poolInfo[j].strat.earn(_msgSender()) {}
+                    try _vaultInfo[j].strat.earn(_msgSender()) {}
                     catch {}
                 }
                 earnMap >>= 1; //shift away the used bit
@@ -218,48 +218,48 @@ abstract contract VaultHealerBase is Ownable, ERC1155Supply, ReentrancyGuard {
             }
         }
     }
-    function earn(uint256 pid) external whenNotPaused(pid) nonReentrant {
-        _poolInfo[pid].strat.earn(_msgSender());
+    function earn(uint256 vid) external whenNotPaused(vid) nonReentrant {
+        _vaultInfo[vid].strat.earn(_msgSender());
     }
 
     //Like OpenZeppelin Pausable, but centralized here at the vaulthealer
     ///////////////////////
-    function pause(uint pid) external onlyOwner {
-        _pause(pid);
+    function pause(uint vid) external onlyOwner {
+        _pause(vid);
     }
-    function unpause(uint pid) external onlyOwner {
-        _unpause(pid);
+    function unpause(uint vid) external onlyOwner {
+        _unpause(vid);
     }
-    function panic(uint pid) external onlyOwner {
-        _pause(pid);
-        _poolInfo[pid].strat.panic();
+    function panic(uint vid) external onlyOwner {
+        _pause(vid);
+        _vaultInfo[vid].strat.panic();
     }
-    function unpanic(uint pid) external onlyOwner {
-        _unpause(pid);
-        _poolInfo[pid].strat.unpanic();
+    function unpanic(uint vid) external onlyOwner {
+        _unpause(vid);
+        _vaultInfo[vid].strat.unpanic();
     }
     
     function paused(address _strat) external view returns (bool) {
-        return paused(findPid(_strat));
+        return paused(findvid(_strat));
     }
-    function paused(uint pid) public view returns (bool) {
-        return !pauseMap.get(pid);
+    function paused(uint vid) public view returns (bool) {
+        return !pauseMap.get(vid);
     }
-    modifier whenNotPaused(uint pid) {
-        require(!paused(pid), "Pausable: paused");
+    modifier whenNotPaused(uint vid) {
+        require(!paused(vid), "Pausable: paused");
         _;
     }
-    modifier whenPaused(uint pid) {
-        require(paused(pid), "Pausable: not paused");
+    modifier whenPaused(uint vid) {
+        require(paused(vid), "Pausable: not paused");
         _;
     }
-    function _pause(uint pid) internal virtual whenNotPaused(pid) {
-        pauseMap.unset(pid);
-        emit Paused(pid);
+    function _pause(uint vid) internal virtual whenNotPaused(vid) {
+        pauseMap.unset(vid);
+        emit Paused(vid);
     }
-    function _unpause(uint pid) internal virtual whenPaused(pid) {
-        require(pid > 0 && pid < _poolInfo.length, "invalid pid");
-        pauseMap.set(pid);
-        emit Unpaused(pid);
+    function _unpause(uint vid) internal virtual whenPaused(vid) {
+        require(vid > 0 && vid < _vaultInfo.length, "invalid vid");
+        pauseMap.set(vid);
+        emit Unpaused(vid);
     }
 }
