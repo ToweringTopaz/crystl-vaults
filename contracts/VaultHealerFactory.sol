1094a5e6f6dc4a438b4745045544970bef27c828
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2022-01-08 01:56:20 -0600

    Update VaultHealerFactory.sol

--- contracts/VaultHealerFactory.sol
+++ contracts/VaultHealerFactory.sol
@@ -17,7 +17,7 @@ abstract contract VaultHealerFactory is VaultHealerBoostedPools {
 //    "60868061000e600039806000f3fe3660008181823773"
 //    7979797979797979797979797979797979797979"331415603757633074440c813560e01c141560335733ff5b8091505b303314156042578091505b8082801560565782833685305afa91506074565b8283368573bebebebebebebebebebebebebebebebebebebebe5af491505b503d82833e806081573d82fd5b503d81f3"
 
-    function getProxyData() external returns (address _implementation, bytes memory _metadata){
+    function getProxyData() external returns (address _implementation, bytes memory _metadata) {
         _implementation = proxyImplementation;
         _metadata = proxyMetadata;
         delete proxyImplementation;
@@ -34,6 +34,7 @@ abstract contract VaultHealerFactory is VaultHealerBoostedPools {
         IERC20[] calldata _earned,
         address _targetVault //maximizer target
     ) external onlyRole("VAULT_ADDER") {
+        proxyImplementation = _implementation;
         address newStrat = Create2.deploy(0, bytes32(_vaultInfo.length), type(VHStrategyProxy).creationCode);
         IStrategy(newStrat).initialize(_wantToken, _masterchefAddress, _tacticAddress, _pid, _settings, _earned, _targetVault);
         addVault(newStrat, _settings.minBlocksBetweenEarns);
