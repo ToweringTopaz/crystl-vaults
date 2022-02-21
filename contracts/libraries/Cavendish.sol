b943b4ae0cfc4d1bda9b6a2fc275329e3358acaf
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2022-02-21 05:58:33 -0600

    docs

--- contracts/libraries/Cavendish.sol
+++ contracts/libraries/Cavendish.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.9;
 
-/*
-Deploys ERC-1167 compliant minimal proxies whose address is determined only by a salt, not the implementation target
+/// @title Cavendish clones
+/// @author ToweringTopaz
+/// @notice Creates ERC-1167 minimal proxies whose addresses depend only on the salt and deployer address
+/// @dev See _fallback for important instructions
 
-Proxy init bytecode: 
+library Cavendish {
+
+/*
+    Proxy init bytecode: 
 
     11 bytes: 602d80343434335afa15f3
 
@@ -20,17 +25,18 @@ Proxy init bytecode:
     f3 return         : 
 
 */
-
-library Cavendish {
-
-    bytes11 constant PROXY_INIT_CODE = hex'602d80343434335afa15f3';
-                                              //keccak256(abi.encodePacked(PROXY_INIT_CODE));
+    
+	bytes11 constant PROXY_INIT_CODE = hex'602d80343434335afa15f3';	//below is keccak256(abi.encodePacked(PROXY_INIT_CODE));
     bytes32 constant PROXY_INIT_HASH = hex'577cbdbf32026552c0ae211272febcff3ea352b0c755f8f39b49856dcac71019';
 
-    //Creates an 1167-compliant minimal proxy whose address is purely a function of the deployer address and the salt
-    function clone(address _implementation, bytes32 salt) internal returns (address instance) {
 
+    ///Creates an 1167-compliant minimal proxy whose address is purely a function of the deployer address and the salt
+    /// @param _implementation The contract to be cloned
+    /// @param salt Used to determine and calculate the proxy address
+    /// @return Address of the deployed proxy
+    function clone(address _implementation, bytes32 salt) internal returns (address) {
         require(_implementation != address(0), "ERC1167: zero address");
+        address instance;
         assembly {
             sstore(PROXY_INIT_HASH, shl(96, _implementation)) //store at slot PROXY_INIT_HASH which should be empty
             mstore(0, PROXY_INIT_CODE)
@@ -38,6 +44,7 @@ library Cavendish {
             sstore(PROXY_INIT_HASH, 0) 
         }
         require(instance != address(0), "ERC1167: create2 failed");
+        return instance;
     }
     
     //Standard function to compute a create2 address deployed by this address, but not impacted by the target implemention
@@ -53,16 +60,23 @@ library Cavendish {
         bytes32 _data = keccak256(abi.encodePacked(bytes1(0xff), deployer, salt, PROXY_INIT_HASH));
         return address(uint160(uint256(_data)));
     }
-
-    //Deploying contracts must call this in fallback(). Will return to the external caller if clone is in progress; otherwise returns internally doing nothing
+	
+	
+    /// @notice Called by the proxy constructor to provide the bytecode for the final proxy contract. 
+    /// @dev Deployer contracts must call Cavendish._fallback() in their own fallback functions.
+    ///      Generally compatible with contracts that use fallback functions. Simply call this at the
+    ///       top of your fallback, and it will run only when needed.
     function _fallback() internal view {
         assembly {
-            let _implementation := sload(PROXY_INIT_HASH)
-            if gt(_implementation, 0) {
-                mstore(0x00, 0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000)
-                mstore(0x0a, _implementation)
-                mstore(0x1e, 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-                return(0x00, 0x2d)
+            if iszero(extcodesize(caller())) { //will be, for a contract under construction
+                let _implementation := sload(PROXY_INIT_HASH)
+                if gt(_implementation, 0) {
+                    mstore(0x00, 0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000)
+                    mstore(0x0a, _implementation)
+                    mstore(0x1e, 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+                    return(0x00, 0x2d) //Return to external caller, not to any internal function
+                }
+
             }
         }
     }
