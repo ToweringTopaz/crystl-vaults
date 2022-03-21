7e955a451dcd98bc166ae47255cc17df32eee80d
Richard Jamieson <jamieson.rich@gmail.com>
2022-03-21 17:28:55 +0200

    createing strat deploy script

--- migrations/1_deploy_v3_strategy.js
+++ migrations/1_deploy_v3_strategy.js
@@ -16,33 +16,38 @@ const QuartzUniV2Zap = artifacts.require("QuartzUniV2Zap");
 withdrawFee = ethers.BigNumber.from(10);
 earnFee = ethers.BigNumber.from(500);
 
-module.exports = async function (deployer, network, accounts) {
-	await deployer.deploy(VaultHealer, "", ZERO_ADDRESS, accounts[0])
-	vaultHealer = await VaultHealer.deployed();
+let [tacticsA, tacticsB] = await tactics.generateTactics(
+	dfynVaults[0]['masterchef'],
+	dfynVaults[0]['PID'],
+	0, //position of return value in vaultSharesTotal returnData array - have to look at contract and see
+	ethers.BigNumber.from("0x70a0823130000000"), //vaultSharesTotal - includes selector and encoded call format
+	ethers.BigNumber.from("0xa694fc3a40000000"), //deposit - includes selector and encoded call format
+	ethers.BigNumber.from("0x2e1a7d4d40000000"), //withdraw - includes selector and encoded call format
+	ethers.BigNumber.from("0x3d18b91200000000"), //harvest - includes selector and encoded call format
+	ethers.BigNumber.from("0xe9fad8ee00000000") //emergency withdraw - includes selector and encoded call format
+);
 
-	await deployer.deploy(VaultFeeManager, 
-		vaultHealer.address, 
-		FEE_ADDRESS, 
-		withdrawFee, 
-		[ FEE_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS ], 
-		[earnFee, 0, 0])
-	vaultFeeManager = await VaultFeeManager.deployed();
 
-    await deployer.deploy(StrategyConfig);
-	const strategyConfigInstance = await StrategyConfig.deployed();
+DEPLOYMENT_DATA = await strategyConfig.generateConfig(
+	tacticsA,
+	tacticsB,
+	dfynVaults[0]['want'],
+	dfynVaults[0]['wantDust'],
+	LP_AND_EARN_ROUTER, //note this has to be specified at deployment time
+	magnetite.address,
+	240, //slippageFactor
+	false, //feeOnTransfer
+	dfynVaults[0]['earned'],
+	dfynVaults[0]['earnedDust'],
+);
 
-    await deployer.deploy(Tactics);
-	const tacticsInstance = await Tactics.deployed();
-    
-	await deployer.deploy(Strategy, vaultHealer.address); //vaultHealer.address
-	const strategyImplementation = await Strategy.deployed();
+await vaultHealer.connect(vaultHealerOwnerSigner).createVault(strategyImplementation.address, DEPLOYMENT_DATA);
 
-	// await deployer.deploy(BoostPool, vaultHealer.address);
-	// const boostPoolImplementation = await BoostPool.deployed();
+strat1_pid = await vaultHealer.numVaultsBase();
 
 	console.log("vaultHealer: ", vaultHealer.address);
 	console.log("VaultFeeManager: ", vaultFeeManager.address);
 	console.log("StrategyConfig: ", strategyConfigInstance.address);
 	console.log("Tactics: ", tacticsInstance.address);
 	console.log("Strategy: ", strategyImplementation.address);
-};
+
