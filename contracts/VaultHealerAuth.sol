a7685c4b0aa79e6f188149f1147e8872e969891f
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2022-03-28 04:09:15 -0500

    create easy permissions functions

--- contracts/VaultHealerAuth.sol
+++ contracts/VaultHealerAuth.sol
@@ -7,48 +7,40 @@ contract VaultHealerAuth is AccessControlEnumerable {
 
     bytes32 constant FEE_SETTER = keccak256("FEE_SETTER");
 
-    constructor(address _owner) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _owner);
-        _setupRole(FEE_SETTER, _owner);
-        _setupRole(bytes4(keccak256("createVault(address,bytes)")), _owner);
-        _setupRole(bytes4(keccak256("createMaximizer(uint256,bytes)")), _owner);
-        _setupRole(bytes4(keccak256("createBoost(uint256,address,bytes)")), _owner);
-        _setupRole(bytes4(keccak256("setAutoEarn(uint256,bool,bool)")), _owner);
-        _setupRole(bytes4(keccak256("unpause(uint256)")), _owner);
-        _setupRole(bytes4(keccak256("pause(uint256,bool)")), _owner);
-        _setupRole(bytes4(keccak256("setURI(string)")), _owner);
+    constructor(address owner) {
+        _setAccess(owner, 3);
     }
 
     function setAccess(address account, uint level) external onlyRole(DEFAULT_ADMIN_ROLE) {
-
-        if (level > 0) {
-            _grantRole(bytes4(keccak256("setAutoEarn(uint256,bool,bool)")), account);
-            _grantRole(bytes4(keccak256("unpause(uint256)")), account);
-            _grantRole(bytes4(keccak256("pause(uint256,bool)")), account);
-        } else {
-            _revokeRole(bytes4(keccak256("setAutoEarn(uint256,bool,bool)")), account);
-            _revokeRole(bytes4(keccak256("unpause(uint256)")), account);
-            _revokeRole(bytes4(keccak256("pause(uint256,bool)")), account);
-        }
-        if (level > 1) {
-            _grantRole(bytes4(keccak256("createVault(address,bytes)")), account);
-            _grantRole(bytes4(keccak256("createMaximizer(uint256,bytes)")), account);
-            _grantRole(bytes4(keccak256("createBoost(uint256,address,bytes)")), account);
-        } else {
-            _revokeRole(bytes4(keccak256("createVault(address,bytes)")), account);
-            _revokeRole(bytes4(keccak256("createMaximizer(uint256,bytes)")), account);
-            _revokeRole(bytes4(keccak256("createBoost(uint256,address,bytes)")), account);            
-        }
-        if (level > 2) {
-            _grantRole(DEFAULT_ADMIN_ROLE, account);
-            _grantRole(FEE_SETTER, account);
-            _grantRole(bytes4(keccak256("setURI(string)")), account);
-        } else {
-            _revokeRole(DEFAULT_ADMIN_ROLE, account);
-            _revokeRole(FEE_SETTER, account);
-            _revokeRole(bytes4(keccak256("setURI(string)")), account);
-            
-        }
+        _setAccess(account, level);
     }
 
+    //Sets an account's roles to match a predefined tiered list, with 3 being the highest level. These correspond to VaultHealer function selectors.
+    function _setAccess(address account, uint level) private {
+
+        if (level > 3) revert("Invalid access level");
+
+        function(bytes32,address) update = _revokeRole;
+
+        if (level == 3) update = _grantRole; //Owner-level access, controlling fees and permissions
+
+        update(DEFAULT_ADMIN_ROLE, account);
+        update(FEE_SETTER, account);
+        update(bytes4(keccak256("setURI(string)")), account);
+
+        if (level == 2) update = _grantRole; //Vault creators
+
+        update(bytes4(keccak256("createVault(address,bytes)")), account);
+        update(bytes4(keccak256("createMaximizer(uint256,bytes)")), account);
+        update(bytes4(keccak256("createBoost(uint256,address,bytes)")), account);            
+
+        if (level == 1) update = _grantRole; //Pausers
+
+        update(bytes4(keccak256("setAutoEarn(uint256,bool,bool)")), account);
+        update(bytes4(keccak256("unpause(uint256)")), account);
+        update(bytes4(keccak256("pause(uint256,bool)")), account);
+
+    }
+
+    function _noop(address, uint) private pure {}
 }
