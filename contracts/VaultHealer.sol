687a6764c5446915e722b907e6c38a982c76e4ce
sparklingGibbon <87142001+sparklingGibbon@users.noreply.github.com>
2022-04-28 20:18:23 -0500

    include frontend functions

--- contracts/VaultHealer.sol
+++ contracts/VaultHealer.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "./QuartzUniV2Zap.sol";
 import "./VaultHealerBoostedPools.sol";
 import "@openzeppelin/contracts/utils/Multicall.sol";
-
+import "./VaultDeploy.sol";
 
 contract VaultHealer is VaultHealerBoostedPools, Multicall {
 
@@ -14,6 +14,8 @@ contract VaultHealer is VaultHealerBoostedPools, Multicall {
         ERC1155("")
     {
         zap = new QuartzUniV2Zap(address(this));
+
+        address(new VaultDeploy());
     }
 
    function isApprovedForAll(address account, address operator) public view override returns (bool) {
@@ -24,10 +26,35 @@ contract VaultHealer is VaultHealerBoostedPools, Multicall {
         _setURI(_uri);
     }
 
-    function stakedWantTokens(address account, uint vid) external view returns (uint) {
-        uint shares = balanceOf(account, vid);
-        return shares == 0 ? 0 : strat(vid).wantLockedTotal() * shares / totalSupply[vid];
+    function stakedWantTokens(address account, uint vid) public view returns (uint) {
+        uint shares = account == address(0) ? 0 : balanceOf(account, vid);
+        return shares == 0 ? 0 : wantLockedTotal(vid) * shares / totalSupply[vid];
     }
+    
+    function tokenData(address account, uint[] calldata vids) external view returns (uint[4][] memory data) {
+        /*[0]: user staked want tokens
+          [1]: user shares (balanceOf)
+          [2]: wantLockedTotal
+          [3]: totalSupply
+        */
 
+        uint len = vids.length;
+        data = new uint[4][](len);
+
+        for (uint i; i < vids.length; i++) {
+            uint vid = vids[i];
+            uint supply = totalSupply[vid];
+            if (supply == 0) continue;
+            data[i][3] = supply;
+            uint wlt = wantLockedTotal(vid);
+            data[i][2] = wlt;
+            if (account == address(0)) continue;
+            uint bal = balanceOf(account, vid);
+            data[i][1] = bal;
+            data[i][0] = bal * wlt / supply;
+        }
+    }
+    function wantLockedTotal(uint vid) public view returns (uint) {
+        return strat(vid).wantLockedTotal();
+    }
 }
-
