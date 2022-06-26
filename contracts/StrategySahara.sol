df4bc37b03598723acc5789cfca12d92690c0668
sparklingGibbon <87142001+ToweringTopaz@users.noreply.github.com>
2022-06-26 14:41:37 -0500

    support MMF/sahara

--- contracts/StrategySahara.sol
+++ contracts/StrategySahara.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.14;
 
-import "./Strategy.sol";
+import "./StrategyCustomRouter.sol";
 import "./interfaces/ISaharaDaoStaking.sol";
 
-contract StrategySahara is Strategy {
-    using StrategyConfig for StrategyConfig.MemPointer;
+contract StrategySahara is StrategyCustomRouter {
 
     ISaharaDaoStaking public constant SaharaStaking = ISaharaDaoStaking(0x6F132536069F8E35ED029CEd563710CF68fE8E54);
 
-    constructor(IVaultHealer _vaultHealer) Strategy(_vaultHealer) {}
+    constructor(IVaultHealer _vaultHealer) StrategyCustomRouter(_vaultHealer) {}
 
     function _vaultHarvest() internal override {
         super._vaultHarvest();
