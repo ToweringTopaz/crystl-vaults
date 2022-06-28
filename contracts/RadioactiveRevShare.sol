85e1f54244e24d87e214ca4759e83abe9e2e9925
sparklingGibbon <87142001+ToweringTopaz@users.noreply.github.com>
2022-06-28 00:40:20 -0500

    correct revert string

--- contracts/RadioactiveRevShare.sol
+++ contracts/RadioactiveRevShare.sol
@@ -269,7 +269,7 @@ contract RadioactiveRevShare is Ownable {
     }
 
     function toUint176(uint256 value) internal pure returns (uint176) {
-        require(value <= type(uint176).max, "SafeCast: value doesn't fit in 128 bits");
+        require(value <= type(uint176).max, "SafeCast: value doesn't fit in 176 bits");
         return uint176(value);
     }
 
