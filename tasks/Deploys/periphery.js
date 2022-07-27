588fa5439e65fa5d74fd845af520a170c4fe1c2f
KingD3lta <91097727+KingD3lta@users.noreply.github.com>
2022-07-26 17:02:42 -0700

    general HH config Cleanup
    added a "spawnV3" task that performs programatic deployment and verification of most core v3 contracts as well as general support for some other networks, removed some unused / broken vars from HHConfig and cleaned up some of the tasks folders.

--- tasks/Deploys/periphery.js
+++ tasks/Deploys/periphery.js
@@ -38,3 +38,31 @@ task(
       upgradeableBeacon.address
     );
   });
+
+  task("deployVaultGetter", "Deploys V3VaultGetter")
+  .addParam("vh", "VaultHealer Address")
+  .setAction(async ({vh}) => {
+    const VaultGetterV3 = await ethers.getContractFactory("VaultGetterV3");
+    const vaultGetterV3 = await VaultGetterV3.deploy(vh);
+
+    console.log("VaultGetterV3 Deployed at address:", vaultGetterV3.address);
+    await vaultGetterV3.deployTransaction.wait(1);
+    await hre.run("verify:verify", {
+      address: vaultGetterV3.address,
+      constructorArguments: [vh]
+    });
+  });
+
+  task("multiCallDeploy", "Deploys Multicall").setAction(async () => {
+    const Multicall = await ethers.getContractFactory("contracts/Multicall.sol:Multicall");
+    const multicall = await Multicall.deploy();
+  
+    console.log("Multicall deployed at address:", multicall.address);
+    
+
+    await multicall.deployTransaction.wait(confirms = 1)
+
+    await hre.run("verify:verify", {
+      address: multicall.address,
+    });
+  });
